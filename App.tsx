d3cafb2ad84987f4d27cd407fbcac99d7a83b8ce
Rei Sikk <reix0010@stud.kea.dk>
2024-01-29 16:02:17 +0100

    React native app init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { Todo } from './entities/Todo';
 
 export default function App() {
+  const handleAddTodo = () => {
+    const newTodo = new Todo(todos[todos.length - 1].id + 1, text, false)
+    console.log(newTodo);
+    setTodos((prevState) => [...prevState, newTodo])
+    
+  }
+
+  const [text, onChangeText] = useState('')
+  const [todos, setTodos] = useState<Todo[]>(
+    [
+      new Todo(1, 'Go to the gym', false),
+      new Todo(2, 'Call skat', false),
+      new Todo(3, 'Buy coffee', false)
+    ]
+  )
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+       <Text>ToDo application</Text>
+      <TextInput
+      onChangeText={onChangeText}
+      placeholder="Type your task here"
+      value={text}
+      style={styles.input}
+      >hello again</TextInput>
       <StatusBar style="auto" />
+      <Button 
+      title="Add to list"
+      onPress={handleAddTodo}
+      color="#841584"
+      accessibilityLabel="Learn more about this purple button"
+      ></Button>
     </View>
   );
 }
@@ -16,5 +47,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 200,
   },
 });
